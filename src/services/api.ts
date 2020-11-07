cbab2abde950a5fb2c7301cd92cd2cf17cfb680b
Davi Henry <davi.henry.davett@gmail.com>
2020-11-06 23:34:30 -0400

    terceiro comit front end

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://https://happycg.herokuapp.com',
+    baseURL: 'http://happycg.herokuapp.com',
 })
 
 export default api;
